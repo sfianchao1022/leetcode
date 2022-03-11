c546ca7a2464facd97a3ec1d9fe395d8e2b657ba
sfianchao1022 <30313481+sfianchao@users.noreply.github.com>
2022-03-12 00:14:42 +0800

    first commit

---
+++ src/com/example/Main.java
@@ -0,0 +1,13 @@
+package com.example;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        // 15. 3Sum
+        System.out.println(Solution.threeSum(new int[]{-1,0,1,2,-1,-4}));
+
+        
+    }
+}
---
+++ src/com/example/Solution.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import java.util.List;
+
+public class Solution {
+
+    public static List<List<Integer>> threeSum(int[] nums) {
+
+        return null;
+    }
+
+}
